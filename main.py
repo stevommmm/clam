74989ac5803868b83f2015f7836172a942033197
stevommmm <git@c45y.com>
2015-01-05 13:45:59 +1000

    added user login and template changes for cleaner ui

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ import sys
 import time
 import stat
 import logging
+import hashlib
+from Cookie import SimpleCookie
 from lucid import lucid, request
 from templates import templates
 
@@ -12,11 +14,52 @@ logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 class config:
 	root = os.path.abspath(os.path.dirname(__file__))
 	file_root = os.path.join(root, 'files')
+	secret = 'supersecretstring'
 
 if not os.path.exists(config.file_root):
 	os.mkdir(config.file_root)
 
 application = lucid()
+
+def set_session(req, username):
+	s = username + ":" + req._ENV.get('REMOTE_ADDR', '-')
+	s_hash = hashlib.sha1(s + config.secret).hexdigest()
+
+	session_cookie = SimpleCookie()
+	session_cookie['clamsession'] = s + ":" + s_hash
+	session_cookie['clamsession']["path"] = '/'
+	session_cookie['clamsession']["httponly"] = True
+	req.headers += [("set-cookie", m.OutputString()) for m in session_cookie.values()]
+
+def get_session(req):
+	s = req._ENV.get('HTTP_COOKIE', None)
+	if not s:
+		return None
+	session_cookie = SimpleCookie(s)
+
+	if not 'clamsession' in session_cookie:
+		return None
+
+	username, remote, uhash = session_cookie['clamsession'].value.split(':')
+	s_hash = hashlib.sha1(username + ":" + remote + config.secret).hexdigest()
+	if uhash == s_hash:
+		if remote == req._ENV.get('REMOTE_ADDR', '-'):
+			return username
+
+def auth_user(username, password):
+	pwh = hashlib.sha1(password + config.secret).hexdigest()
+	pwf = os.path.join(config.file_root, username, '.password')
+	if safepath(pwf):
+		return pwh.strip() == open(pwf, 'rb').read().strip()
+	return False
+
+def set_password(username, password):
+	pwh = hashlib.sha1(password + config.secret).hexdigest()
+	pwf = os.path.join(config.file_root, username, '.password')
+	if safepath(pwf):
+		with open(pwf, 'wb') as f:
+			f.write(pwh)
+
 
 def absjoin(*args):
 	return os.path.abspath(
@@ -34,6 +77,8 @@ def softdir(directory):
 
 def listdir(directory):
 	f = os.listdir(directory)
+	if '.password' in f:
+		f.remove('.password')
 	return map(lambda x:(os.path.isdir(os.path.join(directory, x)), x), f)
 
 
@@ -58,6 +103,8 @@ def app_file_uploads(parent, files):
 		if not f.file:
 			return False
 		fn = f.filename
+		if fn.startswith('.'): #dont allow uploading dot files
+			continue
 		filecontent = f.file.read()
 		fullfn = absjoin(parent, fn)
 		if safepath(fullfn, exists=False):
@@ -71,8 +118,34 @@ def rstyle(req):
 	req.headers = [('content-type', 'text/css')]
 	return templates.style()
 
+@application.route('^/login$')
+def page_login(req):
+	if req.method == 'POST':
+		un = req._POST.getvalue('username')
+		pw = req._POST.getvalue('password')
+		if not un or not pw:
+			return templates.login()
+
+		un = un.lower()
+		if auth_user(un, pw):
+			set_session(req, un)
+			req.redirect = '/'
+			return
+
+	return templates.login()
+
 @application.route('^/$')
 def page_index(req):
+	username = get_session(req)
+	if not username:
+		req.redirect = '/login'
+		return
+
+	if 'oldpass' in req._POST and 'newpass' in req._POST:
+		oldp = req._POST.getvalue('oldpass')
+		newp = req._POST.getvalue('newpass')
+		if auth_user(username, oldp):
+			set_password(username, newp)
 	
 	dirname = ''
 	if 'dir' in req._GET:
@@ -89,7 +162,8 @@ def page_index(req):
 
 	# Create absolute path of file or folder, used for security below
 	path = absjoin(
-		config.file_root, 
+		config.file_root,
+		username,
 		dirname,
 		filename,
 	)
@@ -134,7 +208,7 @@ def page_index(req):
 		filelist = listdir(path)
 		content = []
 		# If we are in the root don't add a "parent" option
-		if not path == config.file_root:
+		if not path == absjoin(config.file_root, username):
 			content.append(templates.parent(dirname=p_dirname, filename='..'))
 
 		for f in filelist:
@@ -145,6 +219,7 @@ def page_index(req):
 
 		return templates.index(
 			title=os.sep if not dirname else dirname,
+			username=username,
 			content=templates.filelist(content='\n'.join(content))
 		)
 
