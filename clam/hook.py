3802945dc0e0ef353c836898ede24b83d3f6b6a2
stevommmm <git@c45y.com>
2015-02-11 14:02:43 +1000

    ext hooks

---
+++ clam/hook.py
@@ -0,0 +1,18 @@
+import collections
+import os
+from clam import logger
+
+hooks = collections.defaultdict(list)
+
+
+def register(event_name, func):
+	logger.info('Registered "%s" for event "%s:%s"', event_name, func.__name__, func.func_code.co_filename.split(os.sep)[-1])
+	hooks[event_name].append(func)
+
+
+def call(event_name, req, *args):
+	retdata = []
+	for x in hooks[event_name]:
+		logger.debug('Dispatched "%s" to "%s:%s" with args %s', event_name, x.__name__, x.func_code.co_filename.split(os.sep)[-1], str(args))
+		retdata.append(x(req, *args))
+	return retdata 
