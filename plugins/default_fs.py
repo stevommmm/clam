1a749770150c544d99cf17adb7b82dd16464b931
stevommmm <git@c45y.com>
2015-02-19 09:30:01 +1000

    be 2.6 compat

--- plugins/default_fs.py
+++ plugins/default_fs.py
@@ -14,6 +14,9 @@ def humb(size):
         size = size / 1024.0  # apply the division
     return "%.*f %s" % (1, size, suffixes[suffixIndex])
 
+def total_seconds(td):
+	return (td.microseconds + (td.seconds + td.days * 24 * 3600) * 1e6) / 1e6
+
 def safepath(directory, username='', exists=True):
 	assert directory.startswith(config.file_root), "403, Access denied."
 	assert directory.startswith(os.path.join(config.file_root, username)), "403, Access denied. User traversal."
@@ -62,29 +65,23 @@ def default_directory_list(username, path):
 
 def default_file_create(username, parent, file_object):
 	"""Delete file, using format /data/{username}/{path} """
-	print 'stack'
 	if not file_object.file:
 		return False
 	fn = file_object.filename
 	if not fn or fn.startswith('.'): #dont allow uploading dot files
 		return
-	# filecontent = file_object.file.read()
 	full_path = os.path.join(config.file_root, username, parent, fn)
 
 	if safepath(full_path, username, exists=False):
 		with open(full_path, 'wb') as ouf:
 			ts = datetime.datetime.now()
 			logger.debug('Writing file to disk at location "%s"', full_path)
-			# import IPython
-			# IPython.embed()
-
 			while True:
-				x = file_object.file.read(1024)
+				x = file_object.file.read(4096)
 				if not x:
 					break
 				ouf.write(x)
-			# ouf.write(filecontent)
-			logger.info('File write complete for location "%s", in duration "%s"', full_path, (datetime.datetime.now() - ts).total_seconds())
+			logger.info('File write complete for location "%s", in duration "%s"', full_path, total_seconds(datetime.datetime.now() - ts))
 	return True
 
 def default_file_delete(req, username, path):
